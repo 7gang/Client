a9d66139fd76e614831b708cb88c22dbe0e7dd41
steffan christensen <stefc5949@gmail.com>
2019-04-27 16:23:58 +0200

    Document Dbdisplay class

--- src/main/java/client/Dbdisplay.java
+++ src/main/java/client/Dbdisplay.java
@@ -3,21 +3,23 @@ package client;
 import javax.swing.*;
 import java.awt.*;
 
-
+/**
+ * Class for the container that displays selected entries from the list in the class Dbtable
+ */
 public class Dbdisplay extends JPanel{
-	BorderLayout layout = new BorderLayout();
-	JLabel text = new JLabel("Test");
-	Font font = new Font("Helvetica", Font.BOLD,50);
+	BorderLayout layout = new BorderLayout();//Border layout used to center the label component
+	JLabel text = new JLabel("Test");//JLabel text component 
+	Font font = new Font("Helvetica", Font.BOLD,50);//Font used for the JLabel
 
 	Dbdisplay(){
-		setPreferredSize(new Dimension(1020,338));
+		setPreferredSize(new Dimension(1020,338));//Setting the size of the container
 		setMaximumSize(getPreferredSize());
 		setMinimumSize(getPreferredSize());
-		setLayout(layout);
-		text.setHorizontalTextPosition(JLabel.CENTER);
+		setLayout(layout);//Setting the layout for the container
+		text.setHorizontalTextPosition(JLabel.CENTER);//Centering the text for the JLabel
 		text.setHorizontalAlignment(JLabel.CENTER);
-		text.setFont(font);
-		add(text,BorderLayout.CENTER);
-		setBorder(BorderFactory.createLineBorder(Color.black));
+		text.setFont(font);//Setting the font
+		add(text,BorderLayout.CENTER);//Adding the JLabel to the container
+		setBorder(BorderFactory.createLineBorder(Color.black));//Creating a border around the container
 	}
 }
