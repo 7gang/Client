6d122dc20e8d708f3ed29a755718a785903e4583
Mihai Alexandru <mciung18@student.aau.dk>
2019-04-27 16:12:09 +0200

    Add comments MihaiPanel

--- src/main/java/client/MihaiPanel.java
+++ src/main/java/client/MihaiPanel.java
@@ -2,18 +2,27 @@ package client;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Class responsible for drawing the panel containing the 4 buttons of the interface.
+ */
+
 public class MihaiPanel extends JPanel {
 	
+	// initialization of the 4 buttons
 	static JButton b1 = new JButton("Add");
 	static JButton b2 = new JButton("Edit");
 	static JButton b3 = new JButton("Remove");
 	static JButton b4 = new JButton("Refresh");
-	int bDim = 80;
+	// initialization of the button sizes and the distance between the buttons.
+	int bDimX = 150;
+	int bDimY = 60;
 	int bDist = 15;
+	// initialization of the font to be used for the button text.
 	Font font = new Font("Helvetica", Font.PLAIN,20);
 
 	
 		MihaiPanel(){
+			// change the Java look and feel to the standard Windows 10 look and feel.
 			try {
 		            // Set System L&F
 		        UIManager.setLookAndFeel(
@@ -32,20 +41,27 @@ public class MihaiPanel extends JPanel {
 		       // handle exception
 		    }
 			
+			// set the button font to the previously initialized one.
 			b1.setFont(font);
 			b2.setFont(font);
 			b3.setFont(font);
 			b4.setFont(font);
-
+			
+			// set the preffered, maximum and minimum size of the buttons.
 			setPreferredSize(new Dimension(150,338));
 			setMaximumSize(getPreferredSize());
 			setMinimumSize(getPreferredSize());
+			
+			// set the visibility of the panel to true, and its background color to white.
 			setVisible(true);
-			setOpaque(true);
 			setBackground(Color.WHITE);
+			
+			// create a border around the panel and set the layout to a vertical BoxLayout.
 			setBorder(BorderFactory.createLineBorder(Color.black));
 			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 			
+			// add the buttons, the rigid area between the buttons for space 
+			// and a vertical glue that centers the buttons vertically.
 			add(Box.createVerticalGlue());
 			add(b1);
 			add(Box.createRigidArea(new Dimension(1,bDist)));
@@ -55,17 +71,16 @@ public class MihaiPanel extends JPanel {
 			add(Box.createRigidArea(new Dimension(1,bDist)));
 			add(b4);
 			
-			b1.setPreferredSize(new Dimension(150, 60));
-			b2.setPreferredSize(new Dimension(150, 60));
-			b3.setPreferredSize(new Dimension(150, 60));
-			b4.setPreferredSize(new Dimension(150, 60));
+			// set the preffered size and the minimum size of the buttons.
+			b1.setPreferredSize(new Dimension(bDimX, bDimY));
+			b2.setPreferredSize(new Dimension(bDimX, bDimY));
+			b3.setPreferredSize(new Dimension(bDimX, bDimY));
+			b4.setPreferredSize(new Dimension(bDimX, bDimY));
 			
 			b1.setMinimumSize(getPreferredSize());
 			b2.setMinimumSize(getPreferredSize());
 			b3.setMinimumSize(getPreferredSize());
 			b4.setMinimumSize(getPreferredSize());
-			
-			setVisible(true);
 			
 			b1.setAlignmentX(CENTER_ALIGNMENT);
 			b2.setAlignmentX(CENTER_ALIGNMENT);
