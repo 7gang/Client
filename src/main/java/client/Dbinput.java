6bcb8ffcddd39e1d31bd4d8a209ef1137a2121e0
steffan christensen <stefc5949@gmail.com>
2019-04-27 16:54:42 +0200

    Document the Dbinput class

--- src/main/java/client/Dbinput.java
+++ src/main/java/client/Dbinput.java
@@ -2,20 +2,22 @@ package client;
 
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ * Class for the container that gives the user input into the database
+ */
 public class Dbinput extends JPanel {
-	BorderLayout layout = new BorderLayout();
-	static JTextField input = new JTextField(20);
+	BorderLayout layout = new BorderLayout();//Border layout used to center the label component
+	static JTextField input = new JTextField(20);//JTextField component to input text into
 	
 	Dbinput(){
-		setPreferredSize(new Dimension(1020,338));
+		setPreferredSize(new Dimension(1020,338));//Setting the size of the container
 		setMaximumSize(getPreferredSize());
 		setMinimumSize(getPreferredSize());
-		setLayout(layout);
-		add(input, BorderLayout.CENTER);
-		input.setSize(400,40);
-		input.setHorizontalAlignment(JTextField.CENTER);
-		setBorder(BorderFactory.createLineBorder(Color.black));
+		setLayout(layout);//Setting the layout for the container
+		add(input, BorderLayout.CENTER);//Adding the JLabel to the container
+		input.setSize(400,40);//Setting the size for the JTextField
+		input.setHorizontalAlignment(JTextField.CENTER);//Centering the text for the JTextField
+		setBorder(BorderFactory.createLineBorder(Color.black));//Creating a border around the container
 	}
 
 }
